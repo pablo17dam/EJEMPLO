480d121a5e8b9df9fcc3ca0cf62914d851cb4f15
Pablo Ruiz <pablo17dam@gmail.com>
2018-04-17 12:04:34 +0200

    NUEVA VERSION

--- HelloGit/src/HelloGit.java
+++ HelloGit/src/HelloGit.java
@@ -4,6 +4,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("hello Git World");
 		System.out.println("hello Git World V3");
+		System.out.println("hello Git World V4");
 
 	}
 
